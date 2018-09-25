307e3a3ac7c358db1776679920aa90137f4c4902
Javier Arboleda <reivaj49@gmail.com>
2018-09-25 14:47:23 -0500

    Spring Initializr

---
+++ src/main/java/co/org/osso/api/ApiApplication.java
@@ -0,0 +1,12 @@
+package co.org.osso.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+}
