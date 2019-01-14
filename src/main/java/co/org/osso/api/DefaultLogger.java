0f4c27a038fd2ff06fa015e2a2a308be91693e92
Javier Arboleda <reivaj49@gmail.com>
2019-01-14 16:17:31 -0500

    Add info to show in logs

--- src/main/java/co/org/osso/api/DefaultLogger.java
+++ src/main/java/co/org/osso/api/DefaultLogger.java
@@ -2,11 +2,28 @@ package co.org.osso.api;
 
 import org.springframework.stereotype.Component;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.sql.Timestamp;
+
 @Component
 public class DefaultLogger implements Logger {
 
     @Override
     public void log(String message) {
-        System.out.println(message);
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        InetAddress ip = null;
+        long threadId = Thread.currentThread().getId();
+
+        try {
+            ip = InetAddress.getLocalHost();
+            message = timestamp + "  JAAR-INFO [" + threadId + "] " + ip + ": " + message;
+            System.out.println(message);
+
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
     }
 }
